a815e06e71071fc85cffb3dc67e34a016bb8fe0a
jincito <110742524+jincito@users.noreply.github.com>
2025-02-08 20:34:16 -0500

    cleaned instantiation

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,4 +1,15 @@
-// TODO:add dice images
-//      create random number
-//      change the imgs to random dice
-//      change title to display a winner
+var randomNumber = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = `images/dice${randomNumber}.png`;
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = `images/dice${randomNumber2}.png`;
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber > randomNumber2) {
+  document.querySelector("h1").innerHTML = "<strong>Player 1 wins!</strong>";
+} else if (randomNumber < randomNumber2) {
+  document.querySelector("h1").innerHTML = "<strong>Player 2 wins!</strong>";
+} else {
+  document.querySelector("h1").innerHTML = "<strong>Draw</strong>";
+}
