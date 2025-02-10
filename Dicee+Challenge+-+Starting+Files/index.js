cd12e13e9d737e47e1855bf765290ad5bf3179ce
jincito <110742524+jincito@users.noreply.github.com>
2025-02-09 22:20:38 -0500

    feat/interactability

--- Dicee+Challenge+-+Starting+Files/index.js
+++ Dicee+Challenge+-+Starting+Files/index.js
@@ -1,15 +1,48 @@
-var randomNumber = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage = `images/dice${randomNumber}.png`;
-document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage);
+var player1 = prompt("Enter the name for Player 1: ");
+var player2 = prompt("Enter the name for Player 2: ");
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage2 = `images/dice${randomNumber2}.png`;
-document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
-
-if (randomNumber > randomNumber2) {
-  document.querySelector("h1").innerHTML = "<strong>Player 1 wins!</strong>";
-} else if (randomNumber < randomNumber2) {
-  document.querySelector("h1").innerHTML = "<strong>Player 2 wins!</strong>";
-} else {
-  document.querySelector("h1").innerHTML = "<strong>Draw</strong>";
+if (player1 === null) {
+  player1 = "Player 1";
 }
+if (player2 === null) {
+  player2 = "Player 2";
+}
+
+var playerTag1 = document.querySelectorAll("p")[0];
+var playerTag2 = document.querySelectorAll("p")[1];
+playerTag1.textContent = `${player1}`;
+playerTag2.textContent = `${player2}`;
+playerTag1.style.color = "blue";
+playerTag2.style.color = "red";
+
+// var heading = document.firstElementChild.lastElementChild.firstElementChild.firstElementChild;
+var heading = document.querySelectorAll("h1")[0];
+
+
+function roll() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImg1 = `images/dice${randomNumber1}.png`;
+  document.querySelectorAll("img")[0].src = randomDiceImg1;
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImg2 = `images/dice${randomNumber2}.png`;
+  document.querySelectorAll("img")[1].src = randomDiceImg2;
+  if (randomNumber1 >  randomNumber2) {
+    playerTag1.innerHTML = `<strong><u>${player1}</u></strong>`;
+    playerTag2.textContent = `${player2}`;
+    heading.textContent = `${player1} Wins!`;
+    heading.style.color = "blue";
+  } else if (randomNumber2 > randomNumber1) {
+    playerTag2.innerHTML = `<strong><u>${player2}</u></strong>`;
+    playerTag1.textContent = `${player1}`;
+    heading.textContent = `${player2} Wins!`;
+    heading.style.color = "red";
+  } else {
+    heading.textContent = "Draw";
+    heading.style.color = "green";
+    playerTag1.textContent = "💩";
+    playerTag2.textContent = "💩";
+  }
+}
+
+roll();
