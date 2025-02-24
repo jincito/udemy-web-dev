e19dae0a458460a13477fd4ed728bef5f6aea7da
jincito <110742524+jincito@users.noreply.github.com>
2025-02-24 14:14:39 -0500

    completed simon game | check answer functionality

--- Simon Game Challenge/Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge/Simon Game Challenge Starting Files/game.js
@@ -1,22 +1,67 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-$(".btn").on("click", function() {
-  var userChosenColor = $(this).attr('id');
-  userClickedPattern.push(userChosenColor);
-  playSound(userChosenColor);
-  // console.log(userClickedPattern);
+$(document).keypress(function() {
+  if (!started) {
+
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
 });
 
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  playSound(userChosenColor);
+  animatePress(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+    console.log("success")
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(() => {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game Over, Press Any Key to Restart");
+
+    startOver();
+  }
+}
+
 function nextSequence() {
-  var randomNum = Math.floor(Math.random() * 4);
-  var randomChosenColor = buttonColors[randomNum];
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
 
   $(`#${randomChosenColor}`).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColor);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
 
 function playSound(name) {
