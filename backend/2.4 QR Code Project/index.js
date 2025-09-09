5255ac5e14c9cb05d7d25cc71c5d35a70cadad0a
jincito <110742524+jincito@users.noreply.github.com>
2025-09-09 15:54:24 -0400

    completed qr-image inquirer and express init

--- backend/2.4 QR Code Project/index.js
+++ backend/2.4 QR Code Project/index.js
@@ -1,18 +1,21 @@
-/*
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
 import fs from "fs";
 import inquirer from "inquirer";
 import qr from "qr-image";
 
 inquirer
-  .prompt("Enter a URL to generate a QR image: ")
+  .prompt([
+    {
+      name: "url",
+      message: "Enter a URL to generate a QR image: "
+    },
+  ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
-    fs.writeFile("URL.txt", answers, (err) => {
+    const url = answers.url;
+    const qr_svg = qr.image(url, { type: 'svg' });
+    qr_svg.pipe(fs.createWriteStream("qr_code.svg"));
+    fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
+      console.log("The file has been saved!");
     });
   })
   .catch((error) => {
@@ -22,3 +25,8 @@ inquirer
       // Something else went wrong
     }
   });
+/*
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
--- backend/2.4 QR Code Project/solution.js
+++ backend/2.4 QR Code Project/solution.js
@@ -12,8 +12,7 @@ inquirer
   .then((answers) => {
     const url = answers.URL;
     var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
-
+    qr_svg.pipe(fs.createWriteStream("qr_img.svg"));
     fs.writeFile("URL.txt", url, (err) => {
       if (err) throw err;
       console.log("The file has been saved!");
