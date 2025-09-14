5205e4f25b8e2c2a60c4a414c80b69996c442e99
jincito <110742524+jincito@users.noreply.github.com>
2025-09-14 18:07:10 -0400

    middleware

--- backend/3.4 Middleware/index3.js
+++ backend/3.4 Middleware/index3.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next) {
+  console.log(`
+Request method: ${req.method}
+Request URL: ${req.url}`);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
--- backend/3.4 Middleware/index4.js
+++ backend/3.4 Middleware/index4.js
@@ -1,7 +1,31 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function bandNameGenerator(request, response, next) {
+  console.log(request.body);
+  bandName = request.body["street"] + request.body["pet"];
+  next();
+}
+
+app.use(bandNameGenerator);
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/public/index.html" );
+})
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`)
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
